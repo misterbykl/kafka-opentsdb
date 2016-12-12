f08c3defb93ae9ccf8932421069247871d06f6d9
misterbykl <baykalemre@gmail.com>
2016-12-12 21:48:01 +0200

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Main.java
+ * <p>
+ * misterbaykal
+ * <p>
+ * 12/12/16 21:42
+ */
+public class Main {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     *             <p>
+     *             misterbaykal
+     *             <p>
+     *             12/12/16 21:42
+     */
+    public static void main(String[] args) {
+
+    }
+}
